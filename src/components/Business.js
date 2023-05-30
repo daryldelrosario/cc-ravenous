ee599a6bce3548e693606b662ec7b074e45e657d
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-05-30 11:23:21 -0400

    cleanup bloat files and create simple business component

---
+++ src/components/Business.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Business() {
+    return (
+        <div>
+            <div>
+                <img src="https://source.unsplash.com/random/400x300/?food" alt="Food"/>
+            </div>
+            <h2>Business Name</h2>
+            <div>
+                <div>Address</div>
+                <div>City, State Zipcode</div>
+            </div>
+            <div>
+                <span>Category </span>
+                <span>Ratings </span>
+                <span>Review Count </span>
+            </div>
+        </div>
+    );
+}
+
+export default Business;
