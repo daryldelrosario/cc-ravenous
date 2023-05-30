49e87c40d07ab370401c7439d438b933ac618dc4
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-05-30 11:38:18 -0400

    include hardcode template for business component

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,20 +1,32 @@
 import React from 'react';
 
 function Business() {
+    const business = {
+        imageSrc: "https://srouce.unsplash.com/random/400x300/?food",
+        name: "Example Restaurant",
+        address: "123 Demo Street",
+        city: "Alpha",
+        state: "Omega",
+        zipCode: "12345",
+        category: "All You Can Eat",
+        rating: 4.44,
+        reviewCount: 143,
+    };
+
     return (
         <div>
             <div>
-                <img src="https://source.unsplash.com/random/400x300/?food" alt="Food"/>
+                <img src={business.imageSrc} alt={business.name}/>
             </div>
-            <h2>Business Name</h2>
+            <h2>{business.name}</h2>
             <div>
-                <div>Address</div>
-                <div>City, State Zipcode</div>
+                <div>{business.address}</div>
+                <div>{`${business.city}, ${business.state} ${business.zipCode}`}</div>
             </div>
             <div>
-                <span>Category </span>
-                <span>Ratings </span>
-                <span>Review Count </span>
+                <span>{business.category}</span>
+                <span>{`${business.rating} stars`}</span>
+                <span>{`${business.reviewCount} reviews`}</span>
             </div>
         </div>
     );
