4a90432225c6bda3c8d2018da53337fae5988bee
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-06-11 11:51:48 -0400

    complete businesslist component

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Business from '../Business/Business';
+
+function BusinessList() {
+    return (
+        <div>
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+        </div>
+    );
+}
+
+export default BusinessList;
